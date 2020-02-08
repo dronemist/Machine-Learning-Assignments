b350669fcbe97cc238bd0f3c8c32ac19a10ff4f1
dronemist <siddhant.mago@gmail.com>
2020-02-08 22:00:15 +0530

    part 1 completed

--- Assignment 1/q1.py
+++ Assignment 1/q1.py
@@ -3,6 +3,7 @@ from numpy import linalg as lg
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import sys
 import math
 
 # Stopping gradient descent 
@@ -87,13 +88,15 @@ def _gradient(x, y, theta):
 
   return gradient / np.shape(x[:, 0])[0]
 
-def batch_gradient_descent(x, y):
+def batch_gradient_descent(x, y, draw_3d = True):
   """
   Applies batch gradient descent to x and y
   return the parameters learnt 
   """
   new_theta = np.zeros_like(x[0])
-  old_theta = np.zeros_like(x[0])
+  old_theta = new_theta
+  # new_theta = np.array([1, 2])
+  # old_theta = new_theta
 
   old_cost = 0
   new_cost = 0
@@ -101,9 +104,13 @@ def batch_gradient_descent(x, y):
   diff = np.inf
   count = 0
 
-  # Plotting the 3-d mesh 
-  x1 = np.linspace(-0.01, 0.01, 100)
-  y1 = np.linspace(-0.5, 1.5, 100)
+  # Plotting the 3-d mesh
+  if draw_3d:
+    x1 = np.linspace(-0.01, 0.01, 100)
+    y1 = np.linspace(-0.5, 1.5, 100)
+  else:
+    x1 = np.linspace(-1, 1, 100)
+    y1 = np.linspace(-0.5, 2.5, 100)  
   X, Y = np.meshgrid(x1, y1)
   Z = []
 
@@ -114,14 +121,25 @@ def batch_gradient_descent(x, y):
       Z[i].append(_loss(x, y, np.array([x_j, Y[i][j]])))
   Z = np.array(Z)
 
-  # Plotting the mest
-  ax = plt.axes(projection='3d')
-  ax.plot_wireframe(X, Y, Z, color='green', linewidths=0.5)
+  # Plotting the mesh
+  left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+  if draw_3d:
+    ax = plt.axes(projection='3d')
+    ax.plot_wireframe(X, Y, Z, color='green', linewidths=0.5)
+  else:
+    # Plotting the contours
+    f2 = plt.figure(2)
+    ax = f2.add_axes([left, bottom, width, height])
+    cp = ax.contourf(X, Y, Z)
+    plt.colorbar(cp)
+    plt.clabel(cp, inline=1, fontsize=10)
+
 
   # Labels
-  ax.set_xlabel('Theta 1')
-  ax.set_ylabel('Theta 0')
-  ax.set_zlabel('z')
+  ax.set_xlabel('Theta[1]')
+  ax.set_ylabel('Theta[0]')
+  if draw_3d:
+    ax.set_zlabel('Cost')
 
   while diff > EPSILON:
     
@@ -138,7 +156,10 @@ def batch_gradient_descent(x, y):
 
     # Plotting every 10th value
     if count % 10 == 0:
-      ax.scatter3D(new_theta[0], new_theta[1], new_cost)
+      if draw_3d:
+        ax.scatter3D(new_theta[0], new_theta[1], new_cost)
+      else: 
+        ax.scatter(new_theta[0], new_theta[1], marker='o')  
       plt.pause(0.2)
     count += 1
 
@@ -147,14 +168,27 @@ def batch_gradient_descent(x, y):
 
 if __name__ == "__main__":
 
+  # If a 3d graph has to be plotted
+  try:
+    draw_3D_graphs = float(sys.argv[1])
+  except:
+    draw_3D_graphs = True
+
+  # custom learning rate
+  try:
+    learning_rate = float(sys.argv[2])
+  except expression as identifier:
+    learning_rate = 0.01   
+
   # Reading csv file and adding 1 for the intercept term
   acidity = __add_dimension(normalise(read_csv_file('data/q1/linearX.csv')))
   density = read_csv_file('data/q1/linearY.csv')
 
   # Applying gradient descent
-  theta = batch_gradient_descent(acidity, density)
+  LEARNING_RATE = learning_rate
+  theta = batch_gradient_descent(acidity, density, draw_3D_graphs)
 
-  # plot_3D(acidity, density, theta)
+  # Displaying the learnt parameters and graphs
   print(_loss(acidity, density, theta))
   plot_2D(acidity, density, theta)
   print("The line is: {}x + {}".format(*theta))
