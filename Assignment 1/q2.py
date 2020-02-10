80750076a756ef55d6f5398dab5ee051e0797bd4
dronemist <siddhant.mago@gmail.com>
2020-02-10 17:00:00 +0530

    modified convergence

--- Assignment 1/q2.py
+++ Assignment 1/q2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import sys
 import collections
 
-EPSILON = 1e-2
+EPSILON = 1e-3
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 
 def sample_points():
   """
@@ -19,7 +19,7 @@ def sample_points():
   noise = np.random.normal(0, 2**0.5, NUMBER_OF_SAMPLES)
   y = []
   for i in range(0, NUMBER_OF_SAMPLES):
-    y.append(SAMPLE_THETA[0] * 1 + SAMPLE_THETA[1] * x1[i] + SAMPLE_THETA[2] * x2[i])
+    y.append(SAMPLE_THETA[0] * 1 + SAMPLE_THETA[1] * x1[i] + SAMPLE_THETA[2] * x2[i] + noise[i])
   np.savetxt('samples.txt', np.c_[x1, x2, y])
 
 def _loss(x, y, theta, start_index, end_index):
@@ -56,45 +56,53 @@ def stochastic_gradient_descent(x, y, batch_size):
   count = 0
 
   total_samples = np.shape(x[:, 0])[0]
-  buffer = collections.deque([], 1000)
+  threshold = min(2000, int(2 * total_samples / batch_size))
+
+  buffer = np.array([])
 
   # Plotting the mesh
-  ax = plt.axes(projection='3d')
+  # ax = plt.axes(projection='3d')
 
-  # Labels
-  ax.set_xlabel('Theta[0]')
-  ax.set_ylabel('Theta[1]')
-  ax.set_zlabel('Theta[2]')
+  # # Labels
+  # ax.set_xlabel('Theta[0]')
+  # ax.set_ylabel('Theta[1]')
+  # ax.set_zlabel('Theta[2]')
 
   while True:
     for i in range(0, total_samples, batch_size):
       
-      # x_temp = x[i : i + batch_size, :]
-      # y_temp = y[i : i + batch_size]
       # Gradient descent
       new_theta = old_theta - (LEARNING_RATE * _gradient(x, y, old_theta, i, i + batch_size))
 
-      buffer.append(_loss(x, y, new_theta, i, i + batch_size))
-      # Calculating new cost
-      new_cost = np.average(buffer)
-      if count % (total_samples / batch_size) == 0:
-        print("Iteration {} => {}".format(count, new_cost))
+      length = len(buffer)
+      if length == threshold:
+        buffer = buffer[1 : length]
+      buffer = np.append(buffer, _loss(x, y, new_theta, i, i + batch_size))
       
-      diff = abs(new_cost - old_cost)
+      if count % int(total_samples / batch_size) == 0:
+        print("Iteration {} => {}".format(count, new_theta))
 
-      if diff < EPSILON:
-        print("Number of updates: {}".format(count))
-        plt.show()
-        return new_theta
-      # Updating theta
-      old_theta = new_theta
-      old_cost = new_cost
+      # If buffer is not full don't check convergence
+      if count > threshold: 
+        # Calculating new cost
+        new_cost = np.mean(buffer[int(length / 2) : threshold])
+        old_cost = np.mean(buffer[0 : int(length / 2)])
+        
+        diff = abs(new_cost - old_cost)
+
+        if diff < EPSILON:
+          print("Number of updates: {}".format(count))
+          plt.show()
+          return new_theta
 
       # Plotting every 100th value
-      if count % 1 == 0:
-        ax.scatter3D(new_theta[0], new_theta[1], new_theta[2])
-        plt.pause(0.2)
-    
+      # if count % 100 == 0:
+      #   ax.scatter3D(new_theta[0], new_theta[1], new_theta[2])
+      #   plt.pause(0.2)
+      
+      # Updating theta
+      old_theta = new_theta
+
       count += 1    
   return new_theta
 
@@ -105,33 +113,39 @@ if __name__ == "__main__":
     batch_size = int(sys.argv[1])
   except:
     batch_size = 100
+
+  try:
+    train = int(sys.argv[2])
+  except:
+    train = 0
   
   # sample_points()
+  if train == 1:
+    # Reading the data and modifying to desired form
+    k = np.loadtxt('samples.txt')
+    # Shuffling data
+    np.random.shuffle(k)
+    x1 = k[:, 0]
+    x2 = k[:, 1]
+    y = k[:, 2]
+    x0 = np.ones_like(x1)
+    x12 = np.column_stack((x1, x2))
+    x = np.column_stack((x0, x12))
+    
+    # Applying SGD 
+    theta = stochastic_gradient_descent(x, y, batch_size)
+    print(theta)
+    print("Loss on training data set: {}".format(_loss(x, y, theta, 0, np.shape(x[:, 0])[0])))
+    np.savetxt(str(batch_size) + '_theta_values.txt', theta)
 
-  # Reading the data and modifying to desired form
-  k = np.loadtxt('samples.txt')
-  # Shuffling data
-  # np.random.shuffle(k)
-  x1 = k[:, 0]
-  x2 = k[:, 1]
-  y = k[:, 2]
-  x0 = np.ones_like(x1)
-  x12 = np.column_stack((x1, x2))
-  x = np.column_stack((x0, x12))
-  
-  # Applying SGD 
-  # theta = stochastic_gradient_descent(x, y, batch_size)
-  # print(theta)
-  # print(_loss(x, y, theta, 0, np.shape(x[:, 0])[0]))
-
-
+  theta = np.loadtxt(str(batch_size) + '_theta_values.txt')
+  # theta = np.array([3, 1, 2])
   # Testing on given data
   k_test = np.loadtxt('./data/q2/q2test.csv', delimiter=',', skiprows=1)
-  print(k_test)
   x1 = k_test[:, 0]
   x2 = k_test[:, 1]
   y = k_test[:, 2]
   x0 = np.ones_like(x1)
   x12 = np.column_stack((x1, x2))
   x = np.column_stack((x0, x12))
-  print(_loss(x, y, theta, 0, np.shape(x[:, 0])[0]))
+  print("Loss on given dataset: {}".format(_loss(x, y, theta, 0, np.shape(x[:, 0])[0])))
