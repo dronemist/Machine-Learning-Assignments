2adc680ceec94e5f76fed10aaebe2b8eedab7cae
dronemist <siddhant.mago@gmail.com>
2020-02-11 15:35:40 +0530

    current q2

--- Assignment 1/q2.py
+++ Assignment 1/q2.py
@@ -2,6 +2,7 @@ import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import sys
+import math
 import collections
 
 EPSILON = 1e-3
@@ -22,22 +23,24 @@ def sample_points():
     y.append(SAMPLE_THETA[0] * 1 + SAMPLE_THETA[1] * x1[i] + SAMPLE_THETA[2] * x2[i] + noise[i])
   np.savetxt('samples.txt', np.c_[x1, x2, y])
 
-def _loss(x, y, theta, start_index, end_index):
-  loss = 0
-  for i in range(start_index, end_index):
-    mat_mult = np.dot(theta.T, x[i])
-    loss += (y[i] - mat_mult) ** 2
-  return loss / (2 * (end_index - start_index))
+def _loss(x, y, theta):
+  total_parameters = np.shape(x[-1])[0]
+  total_samples = np.shape(x[:, 0])[0]
+  theta = np.reshape(theta, (total_parameters, 1))
+  x = x.T
+  y = np.reshape(y, (1, total_samples))
+  loss = y - np.matmul(theta.T, x)
+  loss = np.sum(np.square(loss))
+  return loss / (2 * total_samples)
 
-def _gradient(x, y, theta, start_index, end_index):    
-  gradient = np.zeros_like(x[0])
-
-  for i in range(start_index, end_index):
-    mat_mult = np.dot(theta.T, x[i])
-    # Calculating the gradient
-    gradient += (y[i] - mat_mult) * (-x[i])
-
-  return gradient / (end_index - start_index)
+def _gradient(x, y, theta):    
+  total_parameters = np.shape(x[-1])[0]
+  total_samples = np.shape(x[:, 0])[0]
+  theta = np.reshape(theta, (total_parameters, 1))
+  y = np.reshape(y, (1, total_samples))
+  gradient = -np.matmul((y - np.matmul(theta.T, x.T)), x)
+  answer = gradient[0] / total_samples
+  return answer
 
 def stochastic_gradient_descent(x, y, batch_size):
   """
@@ -54,10 +57,7 @@ def stochastic_gradient_descent(x, y, batch_size):
   count = 0
 
   total_samples = np.shape(x[:, 0])[0]
-  threshold = min(4000, int(2 * total_samples / batch_size))
-  if batch_size == 1:
-    threshold = 20000 
-  buffer = np.array([])
+  CHECK_N = 2000
 
   # # Plotting the mesh
   # ax = plt.axes(projection='3d')
@@ -70,40 +70,36 @@ def stochastic_gradient_descent(x, y, batch_size):
   while True:
     for i in range(0, total_samples, batch_size):
       
+      x_curr = x[i : i + batch_size, :]		
+      y_curr = y[i : i + batch_size]
+      
       # Gradient descent
-      new_theta = old_theta - (LEARNING_RATE * _gradient(x, y, old_theta, i, i + batch_size))
-
-      length = len(buffer)
-      if length == threshold:
-        buffer = buffer[1 : length]
-      buffer = np.append(buffer, _loss(x, y, new_theta, i, i + batch_size))
+      new_theta = old_theta - (LEARNING_RATE * _gradient(x_curr, y_curr, old_theta))
+      new_cost += _loss(x_curr, y_curr, theta)
+      # new_cost /= 2
       
       if count % int(total_samples / batch_size) == 0:
         print("Iteration {} => {}".format(count, new_theta))
 
-      # If buffer is not full don't check convergence
-      if count > threshold: 
-        # Calculating new cost
-        new_cost = np.mean(buffer[int(length / 2) : threshold])
-        old_cost = np.mean(buffer[0 : int(length / 2)])
-        
+      if count % CHECK_N == 0:
+        # print(new_cost - old_cost)
+        new_cost /= CHECK_N
         diff = abs(new_cost - old_cost)
-
+        old_cost = new_cost
+        new_cost = 0
         if diff < EPSILON:
           print("Number of updates: {}".format(count))
           plt.show()
           return new_theta
 
       # Plotting every 100th value
-      # if count % 100 == 0:
+      # if count % 1 == 0:
       #   ax.scatter3D(new_theta[0], new_theta[1], new_theta[2])
       #   plt.pause(0.2)
       
       # Updating theta
       old_theta = new_theta
-
-      count += 1    
-  return new_theta
+      count += 1
 
 
 if __name__ == "__main__":
@@ -123,7 +119,7 @@ if __name__ == "__main__":
     # Reading the data and modifying to desired form
     k = np.loadtxt('samples.txt')
     # Shuffling data
-    np.random.shuffle(k)
+    # np.random.shuffle(k)
     x1 = k[:, 0]
     x2 = k[:, 1]
     y = k[:, 2]
@@ -133,12 +129,12 @@ if __name__ == "__main__":
     
     # Applying SGD 
     theta = stochastic_gradient_descent(x, y, batch_size)
+    # theta = np.zeros_like(x[0])
     print(theta)
-    print("Loss on training data set: {}".format(_loss(x, y, theta, 0, np.shape(x[:, 0])[0])))
+    print("Loss on training data set: {}".format(_loss(x, y, theta)))
     np.savetxt(str(batch_size) + '_theta_values.txt', theta)
 
   theta = np.loadtxt(str(batch_size) + '_theta_values.txt')
-  # theta = np.array([3, 1, 2])
   # Testing on given data
   k_test = np.loadtxt('./data/q2/q2test.csv', delimiter=',', skiprows=1)
   x1 = k_test[:, 0]
@@ -147,4 +143,4 @@ if __name__ == "__main__":
   x0 = np.ones_like(x1)
   x12 = np.column_stack((x1, x2))
   x = np.column_stack((x0, x12))
-  print("Loss on given dataset: {}".format(_loss(x, y, theta, 0, np.shape(x[:, 0])[0])))
+  print("Loss on given dataset: {}".format(_loss(x, y, theta)))
